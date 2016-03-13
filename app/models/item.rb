6451460dfac24409683ccb96e68c55f998ec6e73
aviflombaum <avi@flombaum.com>
2016-03-12 21:26:54 -0500

    Lecture 3: Marking Items Complete and Incomplete

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,17 @@ class Item < ActiveRecord::Base
   # http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#method-i-belongs_to
   belongs_to :list # Add methods to my model.
   validates :description, :presence => true
+  STATUS = {
+    :incomplete => 0,
+    :complete => 1
+  }
+
+  def complete?
+    self.status == STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STATUS[:incomplete]
+  end
   
 end
